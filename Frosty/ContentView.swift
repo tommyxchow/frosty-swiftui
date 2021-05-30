02a83f2387f9da26720b3e79eff695ad1afed382
Tommy Chow <tommycho@buffalo.edu>
2021-05-30 15:08:55 -0400

    Initial Commit

---
+++ Frosty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Frosty
+//
+//  Created by Tommy Chow on 5/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Frosty/FrostyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrostyApp.swift
+//  Frosty
+//
+//  Created by Tommy Chow on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct FrostyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
