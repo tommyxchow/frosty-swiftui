aec023bfd947c78e3cc7a0c198eadc9774ee0f8b
Tommy Chow <tommycho@buffalo.edu>
2021-06-07 03:34:26 -0400

    Add streamer list view model

---
+++ Frosty/ViewModels/StreamerListViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  StreamerListViewModel.swift
+//  Frosty
+//
+//  Created by Tommy Chow on 6/3/21.
+//
+
+import Foundation
+
+class StreamerListViewModel: ObservableObject {
+    @Published var streamers: [StreamerInfo] = []
+    let headers = ["Authorization": "Bearer ", "Client-Id": "k6tnwmfv24ct9pzanhnp2x1yht30oi"]
+    
+    func updateFollowedStreamers() {
+        Request.perform(.GET, to: URL(string: "https://api.twitch.tv/helix/streams/followed?user_id=")!, headers: headers) { data in
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            let result = try! decoder.decode(StreamerData.self, from: data)
+            
+            DispatchQueue.main.async {
+                self.streamers = result.data
+                self.loadThumbnails()
+            }
+        }
+    }
+    
+    func updateTopStreamers() {
+        Request.perform(.GET, to: URL(string: "https://api.twitch.tv/helix/streams")!, headers: headers) { data in
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            let result = try! decoder.decode(StreamerData.self, from: data)
+            
+            DispatchQueue.main.async {
+                self.streamers = result.data
+                self.loadThumbnails()
+            }
+        }
+    }
+    
+    func loadThumbnails() {        
+        for i in streamers.indices {
+            let url = streamers[i].thumbnailUrl.replacingOccurrences(of: "-{width}x{height}", with: "")
+            Request.perform(.GET, to: URL(string: url)!) { data in
+                DispatchQueue.main.async {
+                    self.streamers[i].thumbnail = data
+                }
+            }
+        }
+    }
+}
