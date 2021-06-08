2f6bdc838e8a5166213108c83692021617314456
Tommy Chow <tommycho@buffalo.edu>
2021-06-08 00:54:17 -0400

    Update user model due to JSON decoding error

--- Frosty/Models/User.swift
+++ Frosty/Models/User.swift
@@ -14,12 +14,12 @@ struct UserData: Decodable {
 struct User: Decodable {
     let id : String
     let login: String
+    let displayName: String
     let type: String
     let broadcasterType: String
     let description: String
     let profileImageUrl: String
     let offlineImageUrl: String
     let viewCount: Int
-    let email: String
     let createdAt: String
 }
