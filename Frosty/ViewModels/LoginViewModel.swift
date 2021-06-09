de5a4908d4ba40c3702cebc4e661f8b8e2a264ee
Tommy Chow <tommycho@buffalo.edu>
2021-06-09 14:25:27 -0400

    Move login handler from auth to its own file

---
+++ Frosty/ViewModels/LoginViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewModel.swift
+//  Frosty
+//
+//  Created by Tommy Chow on 6/8/21.
+//
+
+import Foundation
+import AuthenticationServices
+
+class LoginViewModel: NSObject, ObservableObject {   
+    func login(clientID: String, completionHandler: @escaping (String) -> Void) {
+        var newLoginUrl = URLComponents()
+        newLoginUrl.scheme = "https"
+        newLoginUrl.host = "id.twitch.tv"
+        newLoginUrl.path = "/oauth2/authorize"
+        
+        let clientQuery = URLQueryItem(name: "client_id", value: clientID)
+        let redirectQuery = URLQueryItem(name: "redirect_uri", value: "auth://")
+        let responseQuery = URLQueryItem(name: "response_type", value: "token")
+        let scopesQuery = URLQueryItem(name: "scope", value: "chat:read chat:edit user:read:follows")
+        
+        newLoginUrl.queryItems = [clientQuery, redirectQuery, responseQuery, scopesQuery]
+        
+        let session = ASWebAuthenticationSession(url: newLoginUrl.url!, callbackURLScheme: "auth") { callbackURL, error in
+            guard let callbackURL = callbackURL else {
+                print("ERROR", error!, error!.localizedDescription)
+                return
+            }
+            let fragment = "?" + callbackURL.fragment!
+            let queryItems = URLComponents(string: fragment)?.queryItems
+            let token = queryItems?.filter({ $0.name == "access_token" }).first?.value
+            
+            completionHandler(token!)
+                        
+        }
+        session.presentationContextProvider = self
+        // session.prefersEphemeralWebBrowserSession = true
+        if !session.start() {
+            print("fail")
+        }
+    }
+}
+
+extension LoginViewModel: ASWebAuthenticationPresentationContextProviding {
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        return ASPresentationAnchor()
+    }
+}
